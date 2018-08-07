66dc195a8908206cc6840f8728ecd41e0ff2a643
Andrei Romila <andrei.romila@gmail.com>
2018-08-07 10:20:54 +0200

    Dev

--- Application/Routes.php
+++ Application/Routes.php
@@ -2,16 +2,6 @@
 
 /**
  * 
- * Verb	        URI	                    Action	Route Name
- * ---------------------------------------------------------
- * GET	        /photos	                index	photos.index
- * GET	        /photos/create	        create	photos.create
- * POST	        /photos	                store	photos.store
- * GET	        /photos/{photo}	        show	photos.show
- * GET	        /photos/{photo}/edit	edit	photos.edit
- * PUT/PATCH	/photos/{photo}	        update	photos.update
- * DELETE	    /photos/{photo}	        destroy	photos.destroy
- * 
  * Every route must have at least a method, a route (users/profile)
  * and an action (UserController::index). Optionally you can define 
  * a name as the last parameter (needed if you want to generate the 
